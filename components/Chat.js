15f4e7c181cd75d43f311bd0c5c27522ef46a713
ambrosia-fish <josefameur@gmail.com>
2024-08-20 09:06:47 -0400

    Import and implement useEffect.

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+// Import necessary components from React and React Native
+import React, { useState, useEffect } from 'react';
 
 const Chat = ({ route }) => {
     const { name, backgroundColor } = route.params;
 
+    // Use effect to set initial messages when component mounts
+    useEffect(() => {
+      }, []);
     return (
         <View style={[styles.container, { backgroundColor }]}>
             <Text style={styles.heading}>{name}</Text>
