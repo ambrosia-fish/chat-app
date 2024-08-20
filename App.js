c17526a5b33450e6632b1a0674b9cd1b3e9cb910
ambrosia-fish <josefameur@gmail.com>
2024-08-20 09:05:26 -0400

    Added Comments to App.js

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
+// Import necessary navigation components
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// Import custom components
 import Start from './components/Start';
 import Chat from './components/Chat';
 
+// Create a stack navigator
 const Stack = createNativeStackNavigator();
 
+// Define the main App component
 const App = () => {
   return (
+    // Wrap the entire app in NavigationContainer
     <NavigationContainer>
+      {/* Set up the navigator with Start as the initial route */}
       <Stack.Navigator
         initialRouteName="Start"
       >
+        {/* Define the Start screen */}
         <Stack.Screen
           name="Start"
           component={Start}
         />
+        {/* Define the Chat screen */}
         <Stack.Screen
           name="Chat"
           component={Chat}
@@ -25,6 +33,5 @@ const App = () => {
   );
 }
 
-
-
+// Export the App component as the default export
 export default App;
